235d4ab0be5b79e9412ee42f66cd44a413752e87
houry <2507511047@qq.com>
2021-03-02 00:13:32 +0800

    first init

---
+++ src/main/java/top/houry/netty/bulletscreen/BulletScreenApplication.java
@@ -0,0 +1,13 @@
+package top.houry.netty.bulletscreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletScreenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletScreenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/houry/netty/bulletscreen/BulletScreenApplicationTests.java
@@ -0,0 +1,13 @@
+package top.houry.netty.bulletscreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletScreenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
