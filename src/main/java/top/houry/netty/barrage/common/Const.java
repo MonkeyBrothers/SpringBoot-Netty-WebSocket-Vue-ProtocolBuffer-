921072a593d861afa240bc8385364902443a1b90
houruiyang <houruiyang@58.com>
2021-08-23 16:04:22 +0800

    增加Redis相关的key

--- src/main/java/top/houry/netty/barrage/common/Const.java
+++ src/main/java/top/houry/netty/barrage/common/Const.java
@@ -12,5 +12,13 @@ public interface Const {
          * 弹幕在redis中存放的key
          */
         public static final String BARRAGE_REDIS_LIST_KEY = "BARRAGE:LIST:KEY";
+        /**
+         * 实时在线人数统计key
+         */
+        public static final String REA_TIME_ONLINE_POPULATION_KEY = "REA:TIME:ONLINE:POPULATION:KEY";
+        /**
+         * 历史观看总次数
+         */
+        public static final String BARRAGE_TOTAL_NUMBER_KEY = "BARRAGE:TOTAL:NUMBER:KEY";
     }
 }
