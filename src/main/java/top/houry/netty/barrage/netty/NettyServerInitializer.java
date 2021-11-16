b037f690f391f0a1151121bc1046adaace407c30
houruiyang <houruiyang@58.com>
2021-11-16 09:12:56 +0800

    完善1.0版本

--- src/main/java/top/houry/netty/barrage/netty/NettyServerInitializer.java
+++ src/main/java/top/houry/netty/barrage/netty/NettyServerInitializer.java
@@ -6,13 +6,8 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
-import io.netty.handler.codec.protobuf.ProtobufDecoder;
-import io.netty.handler.codec.protobuf.ProtobufEncoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import io.netty.handler.timeout.IdleStateHandler;
-import top.houry.netty.barrage.proto.BarrageProto;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,12 +25,6 @@ public class NettyServerInitializer extends ChannelInitializer<SocketChannel> {
         pipeline.addLast("chunkedWriteHandler", new ChunkedWriteHandler());
         pipeline.addLast("idleStateHandler", new IdleStateHandler(10, 0, 0, TimeUnit.SECONDS));
         pipeline.addLast("webSocketServerProtocolHandler", new WebSocketServerProtocolHandler("/ws"));
-
-        pipeline.addLast("protobufVarint32FrameDecoder", new ProtobufVarint32FrameDecoder());
-        pipeline.addLast("protobufDecoder", new ProtobufDecoder(BarrageProto.BarrageResponse.getDefaultInstance()));
-        pipeline.addLast("protobufVarint32LengthFieldPrepender", new ProtobufVarint32LengthFieldPrepender());
-        pipeline.addLast("protobufEncoder", new ProtobufEncoder());
-
         pipeline.addLast("nettyServerHandler", new NettyServerHandler());
     }
 }
