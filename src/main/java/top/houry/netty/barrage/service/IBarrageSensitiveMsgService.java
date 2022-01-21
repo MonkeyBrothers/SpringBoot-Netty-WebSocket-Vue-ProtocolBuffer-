6f16caea60329d982f687b339044e606bcd29c83
houruiyang <houruiyang@58.com>
2022-01-21 17:06:04 +0800

    增加敏感词汇接口

---
+++ src/main/java/top/houry/netty/barrage/service/IBarrageSensitiveMsgService.java
@@ -0,0 +1,9 @@
+package top.houry.netty.barrage.service;
+
+/**
+ * @Desc 用来处理敏感词汇的接口
+ * @Author houruiyang
+ * @Date 2022/1/21
+ **/
+public interface IBarrageSensitiveMsgService {
+}
---
+++ src/main/java/top/houry/netty/barrage/service/impl/BarrageSensitiveMsgServiceImpl.java
@@ -0,0 +1,15 @@
+package top.houry.netty.barrage.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import top.houry.netty.barrage.service.IBarrageSensitiveMsgService;
+
+/**
+ * @Desc
+ * @Author houruiyang
+ * @Date 2022/1/21
+ **/
+@Service
+@Slf4j
+public class BarrageSensitiveMsgServiceImpl implements IBarrageSensitiveMsgService {
+}
