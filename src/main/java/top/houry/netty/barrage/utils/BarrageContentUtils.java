e173090cadc2e4b9b63e1ed6993f4d4cb78c9e27
houruiyang <houruiyang@58.com>
2022-02-04 01:33:09 +0800

    增加弹幕信息

--- src/main/java/top/houry/netty/barrage/utils/BarrageContentUtils.java
+++ src/main/java/top/houry/netty/barrage/utils/BarrageContentUtils.java
@@ -9,20 +9,18 @@ import java.util.Random;
  **/
 public class BarrageContentUtils {
     public static String[] context = {
-            "最美的人儿",
             "我们都是追梦人！",
             "做的不错",
             "666666666",
-            "这波操作不亏",
             "哈哈哈哈",
             "新年好啊",
             "OK",
             "太棒了",
             "学习一下",
+            "版本在持续不断的更迭中",
+            "欢迎大家踊跃的加入啊",
             "微信搜索:小猴子的技术笔记",
             "小猴子祝您，虎年大吉，虎虎生威",
-            "项目地址:https://gitee.com/MonkeyBrothers/barrage"
-
     };
 
     public static String getContext(String[] barrage){
